31ab3738abfdef1fd17f31048bdf3437ab2461d5
Raggi <35541698+ragrag@users.noreply.github.com>
2021-01-12 15:16:04 +0200

    jest root dir

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -2,10 +2,11 @@ import { ConnectionOptions } from 'typeorm';
 
 const env = process.env.NODE_ENV || 'development';
 const dbConnectionUrls = {
-  production: process.env.DATABASE_URL,
+  production: process.env.DATABASE_URL_PROD,
   development: process.env.DATABASE_URL_DEV,
   testing: process.env.DATABASE_URL_TEST,
 };
+
 const dbConnection: ConnectionOptions = {
   url: dbConnectionUrls[env],
   type: 'postgres',
